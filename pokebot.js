81d15c228efe94665a2a55c47df075f9faf8c85d
Sergio Fragua Ortega <sfragua@mbp-sfrag.local>
2017-03-13 00:48:00 +0100

    first steps bot

--- pokebot.js
+++ pokebot.js
@@ -1,6 +1,45 @@
+const getGreetings = require('./intents/greetings.js')
+const getInfoPokemon = require('./intents/infopokemon.js')
+
 const config = require('./config.js')
 const restify = require('restify')
 const builder = require('botbuilder')
-const server = restify.createServer().listen(8080)
 
-//Connection to Microsoft Bot Framework
+const recast = require('recastai')
+
+// Connection to Microsoft Bot Framework
+const connector = new builder.ChatConnector({
+  appId: config.appId,
+  appPassword: config.appPassword,
+})
+
+const bot = new builder.UniversalBot(connector)
+
+const INTENTS = {
+    greetings: getGreetings,
+    infopokemon: getInfoPokemon,
+}
+
+// Event when Message received
+// Event when Message received
+bot.dialog('/', (session) => {
+  recastClient.textRequest(session.message.text)
+ .then(res => {
+   const intent = res.intent()
+   const entity = res.get('pokemon')
+   if (intent) {
+       session.send(INTENTS[intent.slug](entity))
+   }
+ })
+ .catch(() => session.send('I need some sleep right now... Talk to me later!'))
+})
+
+// Server Init
+
+const server = restify.createServer()
+server.listen(8080)
+server.post('/', connector.listen())
+
+//Recast bot Connection
+
+const recastClient = new recast.Client(config.recast)
